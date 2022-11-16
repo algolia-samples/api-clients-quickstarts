da5e5be0d0ce4157824373614ab0c42f434f206e
David Bacon <davidpeter.bacon@algolia.com>
2022-11-16 14:21:12 +0000

    change back to env variables

--- javascript/export_and_add_rule_to_index.js
+++ javascript/export_and_add_rule_to_index.js
@@ -2,21 +2,21 @@
 const algoliasearch = require("algoliasearch");
 const dotenv = require("dotenv");
 
-// dotenv.config();
+dotenv.config();
 
-// // Get your Algolia Application ID and (admin) API key from the dashboard: https://www.algolia.com/account/api-keys
-// // and choose a name for your index. Add these environment variables to a `.env` file:
-// const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
-// const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
-// const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
+// Get your Algolia Application ID and (admin) API key from the dashboard: https://www.algolia.com/account/api-keys
+// and choose a name for your index. Add these environment variables to a `.env` file:
+const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
+const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
+const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
 
 // Start the API client
 // https://www.algolia.com/doc/api-client/getting-started/instantiate-client-index/
-const client = algoliasearch("OA5TOH2VFB", "66b73691a7304fe7947039f75192d522");
+const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
 
 // Create an index (or connect to it, if an index with the name `ALGOLIA_INDEX_NAME` already exists)
 // https://www.algolia.com/doc/api-client/getting-started/instantiate-client-index/#initialize-an-index
-const index = client.initIndex("export_rules");
+const index = client.initIndex(ALGOLIA_INDEX_NAME);
 
 
 index.browseRules({
