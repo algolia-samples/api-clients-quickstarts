b4deb9972b77159962ed8bc3fd422670887285a1
elsa lebas <lebas.elsa@gmail.com>
2022-11-23 13:35:17 +0100

    script to generate a search only api key with a rate limit to 100 api calls

--- ruby/generate_key.rb
+++ ruby/generate_key.rb
@@ -10,12 +10,9 @@ ALGOLIA_APP_ID = ENV['ALGOLIA_APP_ID']
 ALGOLIA_API_KEY = ENV['ALGOLIA_API_KEY']
 ALGOLIA_INDEX_NAME = ENV['ALGOLIA_INDEX_NAME']
 
-# Initialize the client
+# Initialize the client and the index
 # https://www.algolia.com/doc/api-client/getting-started/initialize/ruby/?client=ruby#initialize-the-search-client
 client = Algolia::Search::Client.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
-
-# Initialize an index
-# https://www.algolia.com/doc/api-client/getting-started/instantiate-client-index/#initialize-an-index
 index = client.init_index(ALGOLIA_INDEX_NAME)
 
 # Set permissions for API key
@@ -26,14 +23,16 @@ acl = ['search']
 # https://www.algolia.com/doc/api-reference/api-methods/add-api-key/#method-param-maxqueriesperipperhour
 
 opts = {
-    description: "TEST !! Restricted search-only API key",
+    description: "Restricted search-only API key limited to 100 API calls per hour",
     # Rate-limit to 100 requests per hour per IP address
     maxQueriesPerIPPerHour: 100
     }
 
 # Create a new restricted search-only API key
-puts "Creating key new restricted search-only API key ..."
+puts "Creating new restricted search-only API key ..."
 
 res = client.add_api_key(acl, opts)
 
-puts "You can check your Dashboard now ! "
+# Print the new generated search api key
+new_search_api_key = res.raw_response[:key]
+puts "Your new key has been generated successfully : #{new_search_api_key}"
