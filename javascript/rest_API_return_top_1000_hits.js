1e2aaf8714ab32e4e27672fa3c94856fa68bdd19
David Bacon <davidpeter.bacon@algolia.com>
2022-12-04 08:32:53 +0000

    change fetch request to async await

--- javascript/rest_API_return_top_1000_hits.js
+++ javascript/rest_API_return_top_1000_hits.js
@@ -10,7 +10,7 @@ const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
 const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
 const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
 const fs = require("fs");
-
+const { fileURLToPath } = require("url");
 
 // Start the API client
 // https://www.algolia.com/doc/api-client/getting-started/instantiate-client-index/
@@ -20,27 +20,54 @@ const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
 // https://www.algolia.com/doc/api-client/getting-started/instantiate-client-index/#initialize-an-index
 const index = client.initIndex(ALGOLIA_INDEX_NAME);
 
-// Create fetch request to Rest API
-fetch(
-	`https://analytics.algolia.com/2/searches?index=${ALGOLIA_INDEX_NAME}&limit=1000`,
-	{
-		method: 'GET',
-		headers: {
-			"X-Algolia-API-Key": `${ALGOLIA_API_KEY}`,
-            "X-Algolia-Application-Id": `${ALGOLIA_APP_ID}`
+// // Create fetch request to Rest API
 
-		}
-	}
-).then(res => res.json())
-.catch(error => console.error('Error:', error))
-.then(response => fs.writeFile(
+// fetch(
+//   `https://analytics.algolia.com/2/searches?index=${ALGOLIA_INDEX_NAME}&limit=1000`,
+//   {
+//     method: "GET",
+//     headers: {
+//       "X-Algolia-API-Key": `${ALGOLIA_API_KEY}`,
+//       "X-Algolia-Application-Id": `${ALGOLIA_APP_ID}`,
+//     },
+//   }
+// )
+//   .then((res) => res.json())
+//   .catch((error) => console.error("Error:", error))
+//   .then((response) =>
+//     fs.writeFile(
+//       `${ALGOLIA_INDEX_NAME}_top_1000_searches.json`,
+//       JSON.stringify(response),
+//       (err) => {
+//         // In case of a error throw err.
+//         if (err) throw err;
+//       }
+//     )
+//   );
+
+(async () => {
+  const response = await fetch(
+    `https://analytics.algolia.com/2/searches?index=${ALGOLIA_INDEX_NAME}&limit=1000`,
+    {
+      method: "GET",
+      headers: {
+        "X-Algolia-API-Key": `${ALGOLIA_API_KEY}`,
+        "X-Algolia-Application-Id": `${ALGOLIA_APP_ID}`,
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const data = await response.json();
+  console.log("creating JSON file...");
+  fs.writeFile(
     `${ALGOLIA_INDEX_NAME}_top_1000_searches.json`,
-    JSON.stringify(response),
+    JSON.stringify(data),
     (err) => {
       // In case of a error throw err.
       if (err) throw err;
-    })
-  )
-    // console.log('Your top searches are:', response));
-
-
+    }
+  );
+  console.log(`JSON file "${ALGOLIA_INDEX_NAME}_top_1000_searches" created!`);
+})();
