8bfaa24460e34e44d08bac0990f896067901c38b
Jake Pople <jake.pople@algolia.com>
2025-12-05 15:31:51 +0000

    export and then add a rule file updated to JS v5 API quickstart

--- javascript/export_and_add_rule_to_index.js
+++ javascript/export_and_add_rule_to_index.js
@@ -1,10 +1,7 @@
-// Install the API client: https://www.algolia.com/doc/api-client/getting-started/install/javascript/?client=javascript
-const algoliasearch = require("algoliasearch");
-const dotenv = require("dotenv");
-// Requiring fs module in which writeFile function is defined.
-const fs = require("fs");
-
-dotenv.config();
+// Install the API client: https://www.algolia.com/doc/libraries/sdk/install#javascript
+import { algoliasearch } from "algoliasearch";
+import "dotenv/config";
+import * as fs from "fs";
 
 // Get your Algolia Application ID and (admin) API key from the dashboard: https://www.algolia.com/account/api-keys
 // and choose a name for your index. Add these environment variables to a `.env` file:
@@ -13,70 +10,68 @@ const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
 const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
 
 // Start the API client
-// https://www.algolia.com/doc/api-client/getting-started/instantiate-client-index/
+// https://www.algolia.com/doc/libraries/sdk/install#test-your-installation
 const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
 
-// Create an index (or connect to it, if an index with the name `ALGOLIA_INDEX_NAME` already exists)
-// https://www.algolia.com/doc/api-client/getting-started/instantiate-client-index/#initialize-an-index
-const index = client.initIndex(ALGOLIA_INDEX_NAME);
+// Create an index name (or connect to it, if an index with the name `ALGOLIA_INDEX_NAME` already exists)
+// https://www.algolia.com/doc/libraries/sdk/install#test-your-installation
+const indexName = ALGOLIA_INDEX_NAME || "new_index_name";
 
-// Export Rules for this index
-// https://www.algolia.com/doc/api-reference/api-methods/export-rules/
-index
-  .browseRules({
-    // A `batch` callback function that's called on every batch of rules
-    batch: (batch) =>
-      // Export JSON file containing Rules into same directory with prefix of index_name
+(async () => {
+  // Export Rules for this index
+  // https://www.algolia.com/doc/libraries/sdk/methods/search/browse-rules
+  await client.browseRules({
+    indexName,
+    aggregator: (res) =>
       fs.writeFile(
         `${ALGOLIA_INDEX_NAME}_rules.json`,
-        JSON.stringify(batch),
+        JSON.stringify(res.hits),
         (err) => {
           // In case of a error throw err.
           if (err) throw err;
         }
       ),
-  })
-  .then((response) => {
-    // Success message
-    console.log(
-      `Rules saved as ${ALGOLIA_INDEX_NAME}_rules.json in the current directory`
-    );
-  })
-  .catch((error) => console.log(error));
+  });
 
-// Create a rule
-const rule = {
-  objectID: "a-rule-id",
-  conditions: [
-    {
-      pattern: "Jimmie",
-      anchoring: "is",
+  console.log(
+    `Rules saved as ${ALGOLIA_INDEX_NAME}_rules.json in the current directory`
+  );
+
+  // Create a rule
+  const rule = {
+    objectID: "a-rule-id",
+    conditions: [
+      {
+        pattern: "Jimmie",
+        anchoring: "is",
+      },
+    ],
+    consequence: {
+      params: {
+        filters: "zip_code = 12345",
+      },
     },
-  ],
-  consequence: {
-    params: {
-      filters: "zip_code = 12345",
-    },
-  },
 
-  // Optionally, to disable the rule change to 'false'
-  enabled: true,
+    // Optionally, to disable the rule change to 'false'
+    enabled: true,
 
-  // Optionally, to add valitidy time ranges
-  validity: [
-    {
-      from: Math.floor(Date.now() / 1000),
-      until: Math.floor(Date.now() / 1000) + 10 * 24 * 60 * 60,
-    },
-  ],
-};
+    // Optionally, to add valitidy time ranges
+    validity: [
+      {
+        from: Math.floor(Date.now() / 1000),
+        until: Math.floor(Date.now() / 1000) + 10 * 24 * 60 * 60,
+      },
+    ],
+  };
 
-// Save the Rule.
-index.saveRule(rule).then(() => {
-  // done
-});
+  // Save the Rule, and forward it to all replicas of the index.
+  const response = await client.saveRule({
+    indexName: indexName,
+    objectID: "a-rule-id",
+    forwardToReplicas: true,
+    rule: rule,
+  });
 
-// Save the Rule, and forward it to all replicas of the index.
-index.saveRule(rule, { forwardToReplicas: true }).then(() => {
-  // done
-});
+  // Log the response
+  console.log(response);
+})();
