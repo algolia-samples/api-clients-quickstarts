c29cd8898b65e772ca78bc71f53000ba8ca97115
Jake Pople <jake.pople@algolia.com>
2025-12-22 11:57:29 +0000

    list indices and record size updated for js v5

--- javascript/list_indices_and_record_size.js
+++ javascript/list_indices_and_record_size.js
@@ -1,43 +1,38 @@
-// Install the API client: https://www.algolia.com/doc/api-client/getting-started/install/javascript/?client=javascript
-const algoliasearch = require("algoliasearch");
-const dotenv = require("dotenv");
-// Requiring fs module in which writeFile function is defined.
-const fs = require("fs");
-
-dotenv.config();
+// Install the API client: https://www.algolia.com/doc/libraries/sdk/install#javascript
+import { algoliasearch } from "algoliasearch";
+import "dotenv/config";
+import * as fs from "fs";
 
 // Get your Algolia Application ID and (admin) API key from the dashboard: https://www.algolia.com/account/api-keys
-// and choose a name for your index. Add these environment variables to a `.env` file:
+// Add these environment variables to a `.env` file:
 const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
 const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
 
 // Start the API client
-// https://www.algolia.com/doc/api-client/getting-started/instantiate-client-index/
+// https://www.algolia.com/doc/libraries/sdk/install#test-your-installation
 const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
 
 // List all indices with listIndices method
-// https://www.algolia.com/doc/api-reference/api-methods/list-indices/?client=javascript
+// https://www.algolia.com/doc/libraries/sdk/methods/search/list-indices
 client.listIndices().then(({ items }) => {
     // Loop into each element and extract only index name and record size
     let filteredItems = [];
-    items.forEach(element => filteredItems.push({
+    items.forEach((element) =>
+      filteredItems.push({
         name: element.name,
         entries: element.entries,
-    }));
+      })
+    );
     // Export JSON file containing only name of indices and record size into same directory
-    fs.writeFile(
-        `listIndices.json`,
-        JSON.stringify(filteredItems),
-            (err) => {
-                // In case of a error throw err.
-                if (err) throw err;
-            }
-        );
-    })
-    .then((response) => {
-        // Success message
-        console.log(
-            `Indices name and record size saved as listIndices.json in the current directory`
-        );
-    })
-    .catch((error) => console.log(error));
+    fs.writeFile(`listIndices.json`, JSON.stringify(filteredItems), (err) => {
+      // In case of a error throw err.
+      if (err) throw err;
+    });
+  })
+  .then((response) => {
+    // Success message
+    console.log(
+      `Indices name and record size saved as listIndices.json in the current directory`
+    );
+  })
+  .catch((error) => console.log(error));
