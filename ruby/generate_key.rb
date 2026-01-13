a9e2a5bdee470d9ca294df714fbacccb8c56a861
Jake Pople <jake.pople@algolia.com>
2026-01-13 16:11:30 +0000

    generate key updated for ruby client v3

--- ruby/generate_key.rb
+++ ruby/generate_key.rb
@@ -1,34 +1,32 @@
+# Install the API client: https://www.algolia.com/doc/libraries/sdk/install#ruby
 require 'dotenv/load'
 require 'algolia'
 
-# Algolia client credentials
+# Get your Algolia Application ID and (admin) API key from the dashboard: https://www.algolia.com/account/api-keys
+# and choose a name for your index. Add these environment variables to a `.env` file:
 ALGOLIA_APP_ID = ENV['ALGOLIA_APP_ID']
 ALGOLIA_API_KEY = ENV['ALGOLIA_API_KEY']
 ALGOLIA_INDEX_NAME = ENV['ALGOLIA_INDEX_NAME']
 
-# Initialize the client and the index
-# https://www.algolia.com/doc/api-client/getting-started/initialize/ruby/?client=ruby#initialize-the-search-client
-client = Algolia::Search::Client.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
-index = client.init_index(ALGOLIA_INDEX_NAME)
-
-# Set permissions for API key
-# https://www.algolia.com/doc/api-reference/api-methods/add-api-key/?client=ruby#method-param-acl
-acl = ['search']
-
-# Set the rate limited parameters for API key
-# https://www.algolia.com/doc/api-reference/api-methods/add-api-key/#method-param-maxqueriesperipperhour
+# Initialise the client and connect to it
+# https://www.algolia.com/doc/libraries/sdk/methods/search#ruby
+client = Algolia::SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
 
 opts = {
+    # Add a description
+    # https://www.algolia.com/doc/rest-api/search/add-api-key#body-description
     description: "Restricted search-only API key limited to 100 API calls per hour",
-    # Rate-limit to 100 requests per hour per IP address
+    # Set the rate limited parameters for API key - Rate-limit to 100 requests per hour per IP address
+    # https://www.algolia.com/doc/rest-api/search/add-api-key#body-max-queries-per-ip-per-hour
     maxQueriesPerIPPerHour: 100
-    }
+}
 
 # Create a new restricted search-only API key
 puts "Creating new restricted search-only API key ..."
 
-res = client.add_api_key(acl, opts)
+# Set permissions for API key and generate the key
+res = client.add_api_key(acl: ['search'], **opts)
 
 # Print the new generated search api key
-new_search_api_key = res.raw_response[:key]
+new_search_api_key = res.key
 puts "Your new Search only Rate Limited API Key is: #{new_search_api_key}"
