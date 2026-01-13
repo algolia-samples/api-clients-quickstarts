9b50e45950e389824119b82ba8bf5f37d3b1988d
Jake Pople <jake.pople@algolia.com>
2026-01-13 15:46:09 +0000

    create and export a rule updated for ruby client v3

--- ruby/export_and_add_rule_to_index.rb
+++ ruby/export_and_add_rule_to_index.rb
@@ -1,22 +1,24 @@
+# Install the API client: https://www.algolia.com/doc/libraries/sdk/install#ruby
 require 'algolia'
 require 'dotenv/load'
 require 'json'
 
-# Algolia client credentials
+# Get your Algolia Application ID and (admin) API key from the dashboard: https://www.algolia.com/account/api-keys
+# and choose a name for your index. Add these environment variables to a `.env` file:
 ALGOLIA_APP_ID = ENV['ALGOLIA_APP_ID']
 ALGOLIA_API_KEY = ENV['ALGOLIA_API_KEY']
 ALGOLIA_INDEX_NAME = ENV['ALGOLIA_INDEX_NAME']
 
-# Initialize the client and the index
-# https://www.algolia.com/doc/api-client/getting-started/initialize/ruby/?client=ruby#initialize-the-search-client
-client = Algolia::Search::Client.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
-index = client.init_index(ALGOLIA_INDEX_NAME)
+# Initialise the client and connect to it
+# https://www.algolia.com/doc/libraries/sdk/methods/search#ruby
+client = Algolia::SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
 
 # Create a rule
-# https://www.algolia.com/doc/api-reference/api-methods/save-rule/?client=ruby
+
 puts "Creating a rule ..."
+objectID = 'a-rule-id'
 rule = {
-     objectID: 'a-rule-id',
+     objectID: objectID,
      conditions: [{
        pattern: 'Jimmie',
        anchoring: 'is'
@@ -33,26 +35,28 @@ rule['enabled'] = false
    
 # Optionally, to add validity time ranges
 rule['validity'] = [
-{
-     from: Time.now.to_i,
-     until: (DateTime.now + 10).to_time.to_i,
-}
+     {
+          from: Time.now.to_i,
+          until: (DateTime.now + 10).to_time.to_i,
+     }
 ]
 
 # Save the Rule.
-index.save_rule(rule)
-# Save the Rule and wait the end of indexing.
-index.save_rule!(rule)
-# Save the Rule, and forward it to all replicas of the index.
-response = index.save_rule(rule, { forwardToReplicas: true })
+# https://www.algolia.com/doc/libraries/sdk/methods/search/save-rule
+client.save_rule(ALGOLIA_INDEX_NAME, objectID, rule)
 
+# Or save the Rule, and forward it to all replicas of the index.
+response = client.save_rule(ALGOLIA_INDEX_NAME, objectID, rule, true)
+
+# Wait for asynchronous task to complete
+client.wait_for_task(ALGOLIA_INDEX_NAME, response.task_id)
 
 # Browse rules
-# https://www.algolia.com/doc/api-reference/api-methods/export-rules/
-res = index.browse_rules()
+# https://www.algolia.com/doc/libraries/sdk/methods/search/browse-rules#ruby
+res = client.browse_rules(ALGOLIA_INDEX_NAME)
 
 # Store rules into an array
-rules = res.map {|rule| rule} 
+rules = res.map { |rule| rule } 
 
 if rules.empty?
      puts "No rules are configured for your index yet."
